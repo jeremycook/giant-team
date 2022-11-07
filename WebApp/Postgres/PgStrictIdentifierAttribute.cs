41e3c9c7a41f084222c4600b0a587747ed3b9e8a
Jeremy Cook <jeremycook.co@gmail.com>
2022-11-07 12:36:09 -0700

    Tweak wording of PgStrictIdentiferAttribute error message

--- WebApp/Postgres/PgStrictIdentifierAttribute.cs
+++ WebApp/Postgres/PgStrictIdentifierAttribute.cs
@@ -3,14 +3,14 @@
 namespace WebApp.Postgres
 {
     /// <summary>
-    /// Requires that the field starts with a lowercase letter, and may be followed by numbers, underscores and lowercase letters.
+    /// Requires that the field starts with a lowercase letter, and may be followed by lowercase letters, numbers and underscores.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public class PgStrictIdentifierAttribute : RegularExpressionAttribute
     {
         public PgStrictIdentifierAttribute() : base("^[a-z][a-z0-9_]*$")
         {
-            ErrorMessage = "The {0} field must start with a lowercase letter, and may be followed by numbers, underscores and lowercase letters.";
+            ErrorMessage = "The {0} field must start with a lowercase letter, and may be followed by lowercase letters, numbers and underscores.";
         }
     }
 }
