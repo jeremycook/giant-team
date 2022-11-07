0cbb137092a8702bf5cfc6bfcb70a5fa5ee767ff
Jeremy Cook <jeremycook.co@gmail.com>
2022-11-07 12:36:48 -0700

    Change PgLaxIdentifierAttribute to require a letter as the first character

--- WebApp/Postgres/PgLaxIdentifierAttribute.cs
+++ WebApp/Postgres/PgLaxIdentifierAttribute.cs
@@ -8,9 +8,9 @@ namespace WebApp.Postgres
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public class PgLaxIdentifierAttribute : RegularExpressionAttribute
     {
-        public PgLaxIdentifierAttribute() : base("^[A-Za-z0-9-_ ]*$")
+        public PgLaxIdentifierAttribute() : base("^[a-zA-Z][a-zA-Z0-9-_ ]*$")
         {
-            ErrorMessage = "The {0} field may only contain letters, numbers, dashes, underscores and spaces.";
+            ErrorMessage = "The {0} field may only contain letters, numbers, underscores, dashes and spaces.";
         }
     }
 }
