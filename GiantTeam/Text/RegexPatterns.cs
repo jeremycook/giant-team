88aa9d1a78a4c40f9516d0471126cf0bdd6b483e
Jeremy Cook <jeremycook.co@gmail.com>
2022-11-16 07:06:06 -0700

    Add RegexPatterns.Alpha

---
+++ GiantTeam/Text/RegexPatterns.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GiantTeam.Text
+{
+    public static partial class RegexPatterns
+    {
+        /// <summary>
+        /// Matches if entire line is empty or English letters.
+        /// </summary>
+        public static Regex Alpha { get; } = alpha();
+
+        [GeneratedRegex("^[A-Za-z]*$")]
+        private static partial Regex alpha();
+    }
+}
