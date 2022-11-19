4aa64b156cfdb061c45daf3d3c804e5997a62574
Jeremy Cook <jeremycook.co@gmail.com>
2022-11-19 06:12:31 -0700

    Remove all JS and TS from WebApp

--- WebApp/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-function autofocus() {
-    const source = document.querySelector("[data-autofocus]");
-    if (source) {
-        const autofocusSelector = source.getAttribute("data-autofocus");
-        const target = source.querySelector(autofocusSelector);
-        target.focus();
-    }
-}
-autofocus();
