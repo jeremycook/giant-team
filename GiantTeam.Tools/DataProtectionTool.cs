5f0d1acbdda2995a0a483fda13693a54613951c9
Jeremy Cook <jeremycook.co@gmail.com>
2022-12-06 10:03:55 -0700

    Add dataprotection tool

---
+++ GiantTeam.Tools/DataProtectionTool.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+public class DataProtectionTool
+{
+    public static void DataProtection(DataProtectionFormat format = DataProtectionFormat.Pem)
+    {
+        X500DistinguishedName subject = new X500DistinguishedName("CN=Data Protection Certificate");
+        using var algorithm = RSA.Create(keySizeInBits: 4096);
+        var request = new CertificateRequest(subject, algorithm, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DataEncipherment, critical: true));
+        var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(10));
+        var pfxBytes = certificate.Export(X509ContentType.Pkcs12, string.Empty);
+
+        var textCert =
+        "-----BEGIN PRIVATE KEY-----\n" +
+        string.Concat(Convert.ToBase64String(pfxBytes).Select((ch, i) => ch + (i % 64 == 0 && i > 0 ? "\n" : ""))) + "\n" +
+        "-----END PRIVATE KEY-----";
+
+        switch (format)
+        {
+            case DataProtectionFormat.Json:
+                Console.WriteLine(JsonSerializer.Serialize(textCert));
+                break;
+
+            case DataProtectionFormat.Pem:
+            default:
+                Console.WriteLine(textCert);
+                break;
+        }
+    }
+}
--- GiantTeam.Tools/Program.cs
+++ GiantTeam.Tools/Program.cs
@@ -1,21 +1,33 @@
 ﻿using GiantTeam.Tools;
 using static System.Console;
 
-bool preview = args.Contains("--preview");
-
 switch (args.FirstOrDefault())
 {
     case "typescript":
+        bool preview = args.Contains("--preview");
         TypeScriptTool.TypeScript(preview);
         break;
+
+    case "dataprotection":
+        var format = args.Contains("--format=json") ?
+            DataProtectionFormat.Json :
+            DataProtectionFormat.Pem;
+        DataProtectionTool.DataProtection(format);
+        break;
+
     default:
         WriteLine(Path.GetFileName(Environment.ProcessPath) + " [tool] [--option]*");
         WriteLine();
-        WriteLine("Tools:");
-        WriteLine("typescript   Write TypeScript type files based on .NET types");
+        WriteLine("typescript");
+        WriteLine("  Write TypeScript type files based on .NET types:");
+        WriteLine("typescript --preview");
+        WriteLine("  Preview TypeScript type files based on .NET types in the console");
         WriteLine();
-        WriteLine("Options:");
-        WriteLine("--preview    Preview the output without modifying anything");
+        WriteLine("dataprotection");
+        WriteLine("dataprotection --format=pem");
+        WriteLine("  Generate a data protection certificate and output as PEM to the console");
+        WriteLine("dataprotection --format=json");
+        WriteLine("  Generate a data protection certificate and output as JSON to the console");
         WriteLine();
         break;
 }
