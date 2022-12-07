6c377c60921d94478ba981ed84f25e9676159b1d
Jeremy Cook <jeremycook.co@gmail.com>
2022-12-07 04:27:10 -0700

    Add reflect API

---
+++ GiantTeam.Asp/Controllers/ReflectController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiantTeam.Authentication.Api.Controllers;
+
+[AllowAnonymous]
+public class ReflectController : ControllerBase
+{
+    public class ReflectOutput
+    {
+        public string Scheme { get; set; } = null!;
+        public string Host { get; set; } = null!;
+        public string Protocol { get; set; } = null!;
+        public string? RemoteIpAddress { get; set; } = null!;
+    }
+
+    [HttpGet("/api/[Controller]")]
+    public ReflectOutput Get()
+    {
+        return new()
+        {
+            Scheme = Request.Scheme,
+            Host = Request.Host.Value,
+            Protocol = Request.Protocol,
+            RemoteIpAddress = HttpContext.Connection.RemoteIpAddress?.ToString(),
+        };
+    }
+}
